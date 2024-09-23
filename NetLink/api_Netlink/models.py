49c90a7811703be2c04698c0bf150caa52f747b9
julian <julilito09@gmail.com>
2024-09-23 13:59:51 -0500

    se agrega clase publicacion

--- NetLink/api_Netlink/models.py
+++ NetLink/api_Netlink/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class publicacion(models.Model):
+    titulo=models.CharField(max_length=50)
+    mdescripcion=models.CharField(max_length=140)
+    
